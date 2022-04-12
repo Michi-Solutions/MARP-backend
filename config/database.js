2f81c5ed453ffbc2dc2a4cef944b3ff0f3fa2798
marleyabe <marleyabe@gmail.com>
2022-04-12 10:39:04 -0300

    add env variable

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: 'MARP',
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'MARP',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
 }
