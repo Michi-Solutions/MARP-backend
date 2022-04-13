a2e8c6ac4be177427a6526a5135049becfb76a80
marleyabe <marleyabe@gmail.com>
2022-04-13 12:43:31 -0300

    add tests

--- __test__/test.js
+++ __test__/test.js
@@ -1,8 +1,12 @@
 const app = require('../src/server');
 const request = require('supertest');
 
-describe('GET /', () => {
-    it('should return 200 OK', async () => {
+const RANDOM = Math.random() * 1000
+let TOKEN = ""
+let id = 0
+
+describe('Server test', () => {
+    it('Server run test', async () => {
         const res = await request(app)
             .get('/')
         
@@ -10,3 +14,63 @@ describe('GET /', () => {
         expect(res.body).toHaveProperty('message', 'Hello World');
     });
 });
+
+describe('Authenticate Test', () => {
+    it('Register test', async () => {
+        const res = await request(app)
+            .post('/register')
+            .send({
+                name: 'test',
+                email: `test${RANDOM}@gmail.com`,
+                password: 'pizza123'
+            })
+        
+        expect(res.status).toEqual(201);
+    });
+
+    it('Login test', async () => {
+        const res = await request(app)
+            .post('/login')
+            .send({
+                email: `test${RANDOM}@gmail.com`,
+                password: 'pizza123'
+            })
+
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveProperty('token');
+        TOKEN = res.body.token
+        id = res.body.user.id
+    })
+
+    it('View user test', async () => {
+        const res = await request(app)
+            .get(`/user/${id}`)
+            .set('Authorization', `Token ${TOKEN}`)
+
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveProperty('id');
+        expect(res.body).toHaveProperty('name');
+        expect(res.body).toHaveProperty('email');
+        expect(res.body).toHaveProperty('role');
+    })
+
+    it('Edit user test', async () => {
+        const res = await request(app)
+            .put(`/user/${id}`)
+            .set('Authorization', `Token ${TOKEN}`)
+            .send({
+                name: `testUpdate`,
+            })
+        
+        expect(res.status).toEqual(200);
+
+    })
+
+    it('Delete user test', async () => {
+        const res = await request(app)
+            .delete(`/user/${id}`)
+            .set('Authorization', `Token ${TOKEN}`)
+
+        expect(res.status).toEqual(200);
+    })
+})
