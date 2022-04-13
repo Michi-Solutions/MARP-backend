0efe6aed4cf0ab676f62cd25f7453153f2bf343c
marleyabe <marleyabe@gmail.com>
2022-04-13 11:36:34 -0300

    start server

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const express = require('express');
 
-const app = express();
+const app = require('./src/server');
 
-const userRoutes = require("./src/routes/user.routes");
-
-app.use(express.json());
-
-app.use(userRoutes)
-
-
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
-})
 
 app.listen(3000)
