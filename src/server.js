d27b94e217bd7f1ca641598ba4d2753a13e1fd1f
mayconabe <maycon.abe01@gmail.com>
2022-04-19 12:01:45 -0300

    Fixed cors problem

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-
 const app = express();
-
 const userRoutes = require("./routes/user.routes");
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
-
 app.use(userRoutes)
 
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello World' });
 })
+app.listen(8080, () => console.log("API is running on port 8080"));
 
 module.exports = app;
