d6d5e60e0872307e05f856d7336cb84b3175310d
marleyabe <marleyabe@gmail.com>
2022-04-28 11:41:01 -0300

    add to app AreaConhecimento routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const userRoutes = require("./routes/user.routes");
+const areaConhecimentoRoutes = require("./routes/areaConhecimento.routes");
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes)
+app.use(areaConhecimentoRoutes)
 
 
 app.get('/', (req, res) => {
