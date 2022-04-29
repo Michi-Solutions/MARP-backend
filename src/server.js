a2b973fd5bfd4e15ec786b8ae0c7314ad9d73698
marleyabe <marleyabe@gmail.com>
2022-04-29 13:01:21 -0300

    add form-data

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const formData = require('express-form-data');
 const userRoutes = require("./routes/user.routes");
 const areaConhecimentoRoutes = require("./routes/areaConhecimento.routes");
+const conteudoRoutes = require("./routes/conteudo.routes");
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use(formData.parse());
 app.use(userRoutes)
 app.use(areaConhecimentoRoutes)
+app.use(conteudoRoutes)
 
 
 app.get('/', (req, res) => {
