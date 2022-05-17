d959fe3f2861164a2532052e2b2ac0cb8a10e0de
marleyabe <marleyabe@gmail.com>
2022-05-17 12:58:44 -0300

    remove useless url

--- src/routes/conteudo.routes.js
+++ src/routes/conteudo.routes.js
@@ -3,61 +3,98 @@ const { areaConhecimento } = require('../../app/models');
 const { Conteudo } = require('../../app/models');
 const { User } = require('../../app/models');
 const { Regra } = require('../../app/models');
+const { Turma } = require('../../app/models');
 const jwt = require('jsonwebtoken');
 
 const conteudoRoutes = express.Router();
 
 // list all areasConhecimento
 conteudoRoutes.get('/conteudo', async (req, res) => {
-    //TODO: implementar filtros para acesso do conteudo
 
     const token = req.headers.authorization.split(" ")[1];
     let decoded = jwt.verify(token, 'secret');
+    const conteudos = []
     const _user = await User.findOne({ where: { id_usuario: decoded.id } });
-    
 
-    for(let id_turma = 0; id_turma < _user.dataValues.turmas_area_conhecimento.id_turmas.length  ; id_turma++){
-        const _areaConhecimento = await areaConhecimento.findOne({ where: { id_area_conhecimento: _user.dataValues.turmas_area_conhecimento.id_turmas[id_turma] } });
+    if(_user.perfil == 'student'){
+        const _turma = await Turma.findAll({ where: { id_usuario: decoded.id } })
         
-        const _conteudo = await Conteudo.findAll({ where: { id_area_conhecimento: _areaConhecimento.dataValues.id_area_conhecimento } });
-        console.log(_conteudo);
-        return res.json(_conteudo);
-    }
+        for(let turma = 0; turma < _turma.length  ; turma++){
+            console.log(_turma[turma].dataValues.id_area_conhecimento);
+            const _conteudo = await Conteudo.findAll({ where: { id_area_conhecimento: _turma[turma].dataValues.id_area_conhecimento } })
+            conteudos.push(_conteudo)
+        }
+    }else if(_user.perfil == 'professor'){
+        const _areaConhecimento = await areaConhecimento.findAll({ where: { id_professor: decoded.id } })
+        for(let turma = 0; turma < _areaConhecimento.length  ; turma++){
+            const _conteudo = await Conteudo.findAll({ where: { id_area_conhecimento: _areaConhecimento[turma].dataValues.id_area_conhecimento } })
+            conteudos.push(_conteudo)
+        }
 
-    const _conteudos = await Conteudo.findAll();
-    return res.json(_conteudos);
+    }
+    return res.json(conteudos);
+})
+
+conteudoRoutes.get('/conteudo/:id', async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        let decoded = jwt.verify(token, 'secret');
+
+        const _conteudo = await Conteudo.findOne({ where: { id_conteudo: req.params.id } })
+        const _turma = await Turma.findOne({ where: { id_usuario: decoded.id, id_area_conhecimento: _conteudo.dataValues.id_area_conhecimento  } })
+
+        if (_turma != null) {
+            return res.json(_conteudo);
+        } else {
+            return res.status(401).json({ msg: "You are not authorized to view this page" });
+        }
+    } catch {
+        return res.status(401).json({ msg: "Invalid Credentials" });
+    }
 })
 
 conteudoRoutes.post('/conteudo', async (req, res) => {
-    const conteudo = { titulo, texto, tipo, id_area_conhecimento } = req.body
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        let decoded = jwt.verify(token, 'secret');
+        const _user = await User.findOne({ where: { id_usuario: decoded.id } });
 
-    const regra = { qtd_insercao, data_inicial, data_final } = req.body
+        if (_user.perfil === "professor") {
+            const conteudo = { titulo, texto, tipo, id_area_conhecimento } = req.body
 
-    console.log(req.files.arquivo)
+            const regra = { qtd_insercao, data_inicial, data_final } = req.body
 
-    const _regra = await Regra.create({
-        nome: regra.nome,
-        qtd_insercao_dia: regra.qtd_insercao,
-        data_inicial: Date.parse(regra.data_inicial),
-        data_final: Date.parse(regra.data_final),
-        ativa: true,
-        data_cadastro: new Date(),
-        data_exclusao: null
-    })
+            const _regra = await Regra.create({
+                nome: regra.nome,
+                qtd_insercao_dia: regra.qtd_insercao,
+                data_inicial: Date.parse(regra.data_inicial),
+                data_final: Date.parse(regra.data_final),
+                ativa: true,
+                data_cadastro: new Date(),
+                data_exclusao: null
+            })
 
-    const _conteudo = await Conteudo.create({
-        titulo: conteudo.titulo,
-        texto: conteudo.texto,
-        tipo: conteudo.tipo,
-        data_criacao: new Date(),
-        data_exclusao: null,
-        duracao: null,
-        arquivo: req.files.arquivo,
-        id_area_conhecimento: conteudo.id_area_conhecimento,
-        id_regra: _regra.id_regra,
-    });
+            console.log(_regra.id_regra)
 
-    return res.status(201).json({ msg: "Conteudo created successfully" });
+            const _conteudo = await Conteudo.create({
+                titulo: conteudo.titulo,
+                texto: conteudo.texto,
+                tipo: conteudo.tipo,
+                data_criacao: new Date(),
+                data_exclusao: null,
+                duracao: null,
+                arquivo: req.files.arquivo,
+                id_area_conhecimento: conteudo.id_area_conhecimento,
+                id_regra: _regra.id_regra,
+            });
+            return res.status(201).json({ msg: "Created successfully" });
+        } else {
+            return res.status(401).json({ msg: "Token Invalid" });
+        }
+    } catch {
+        return res.status(401).json({ msg: "Invalid Credentials" });
+    }
+    
 })
 
 module.exports = conteudoRoutes
